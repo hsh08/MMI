a2672e30ebb6b376ea0c93b431d0acb685c1a49f
한상혁 <hansh080831@naver.com>
2023-10-08 16:59:38 +0900

    아이콘 추가하기

--- app/(tabs)/_layout.tsx
+++ app/(tabs)/_layout.tsx
@@ -1,34 +1,66 @@
 import { Tabs } from "expo-router"
 import React from "react"
+import { Entypo } from '@expo/vector-icons';
+import { Ionicons } from '@expo/vector-icons';
+import { FontAwesome5 } from '@expo/vector-icons';
+import { AntDesign } from '@expo/vector-icons';
 
 export default () => {
     return (
         <Tabs>
             <Tabs.Screen 
-                name="chat" 
-                options={{ headerShown: false }}
+                name="dictionary" 
+                options={{ 
+                    tabBarIcon: ({color, size}) => (
+                        <Entypo name="book" size={24} color="black" />
+                    ),
+                    headerShown: false 
+                }}
             />
-            <Tabs.Screen 
-                name="post" 
-                options={{ headerShown: false }}
+            <Tabs.Screen
+                name="post"
+                options={{ 
+                    tabBarIcon: ({color, size}) => (
+                        <AntDesign name="pluscircle" size={24} color="black" />
+                    ),
+                    headerShown: false 
+                }}
             />
             <Tabs.Screen 
                 name="home" 
-                options={
-                    {headerShown: false}
-                }
-            />
-            <Tabs.Screen 
-                name="dictionary" 
-                options={{ headerShown: false }}
-            />
-            <Tabs.Screen 
-                name="profile" 
-                options={{ headerShown: false }}
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Entypo name="home" size={24} color="black" />
+                    ),
+                    headerShown: false
+                }}
             />
             <Tabs.Screen 
                 name="random" 
-                options={{ headerShown: false }}
+                options={{ 
+                    tabBarIcon: ({color, size}) => (
+                        <FontAwesome5 name="dice" size={24} color="black" />
+                    ),
+                    headerShown: false 
+                }}
+            />
+            <Tabs.Screen 
+                name="chat" 
+                options={{
+                    tabBarIcon: ({color, size}) => (
+                        <Entypo name="chat" size={24} color="black" />
+                    ),
+                    headerShown: false 
+                }}
+            />
+            <Tabs.Screen 
+                name="profile" 
+                options={{ 
+                    tabBarIcon: ({color, size}) => (
+                        <Ionicons name="person-circle-sharp" size={24} color="black" />
+                    ),
+                    headerShown: false 
+                }}
             />
         </Tabs>
 
